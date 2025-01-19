54260a07fa4dbd298a67e5ad3e91be3802fc52f3
pulkit1057 <100367696+pulkit1057@users.noreply.github.com>
2025-01-19 10:41:25 +0530

    implemented password encryption

--- portfolio_backend/user_router.js
+++ portfolio_backend/user_router.js
@@ -1,20 +1,22 @@
 const db = require('./demo_db')
+const bcrypt = require('bcrypt')
 
 const router = require('express').Router()
 
 
 router.post('/registeration', async (req, res) => {
     try {
-        const { email, passkey ,name} = req.body
+        const { email, passkey, name } = req.body
 
         const isRegistered = await db.query(`select * from users where email = '${email}'`)
 
-        if(isRegistered[0].length === 1)
-        {
-            return res.json({success:false,message:"Already registered with this email"})
+        if (isRegistered[0].length === 1) {
+            return res.json({ success: false, message: "Already registered with this email" })
         }
 
-        db.query(`insert into users(email,passkey,name) values('${email}','${passkey}','${name}')`).then(() => {
+        const encryptedPassword = await bcrypt.hash(passkey, 10)
+
+        db.query(`insert into users(email,passkey,name) values('${email}','${encryptedPassword}','${name}')`).then(() => {
             return res.json({ success: true, message: "New user created successfully" })
         })
     } catch (err) {
@@ -27,14 +29,24 @@ router.get('/login', async (req, res) => {
     try {
         const { email, passkey } = req.body
 
-        const response = await db.query(`select * from users where email='${email}' and passkey='${passkey}'`)
-        console.log(response);
+        const response = await db.query(`select passkey from users where email = '${email}'`)
+        console.log(response[0][0]['passkey'])
+
         if (response[0].length === 1) {
-            return res.json({ success: true, message: "User logged in successfully" })
+            const isCorrectpassword = await bcrypt.compare(passkey, response[0][0]['passkey'])
+            console.log(isCorrectpassword);
+
+            if (isCorrectpassword) {
+                return res.json({ success: true, message: "User logged in successfully" })
+            }
+            else {
+                return res.json({ success: false, message: "Incorrect password" })
+            }
         }
         else {
             return res.json({ success: false, message: "No such user exists or wrong password" })
         }
+
     } catch (error) {
         console.log(error);
         return res.json({ success: false, err: error })
